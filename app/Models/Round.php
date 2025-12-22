dd23d6f07c45c4c440cea248132db01bccba8813
Muhammad Muneeb <itsmuneebrehman.conact@gmail.com>
2025-12-22 14:31:52 +0500

    Round Migration and model added

---
+++ app/Models/Round.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Round extends Model
+{
+     protected $fillable = [
+        'event_id',
+        'name',
+        'round_number',
+    ];
+
+    public function event()
+    {
+        return $this->belongsTo(Event::class);
+    }
+
+    public function games()
+    {
+        return $this->hasMany(Game::class);
+    }
+}
