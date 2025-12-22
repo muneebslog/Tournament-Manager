5dfb484bd0a651e3387130e0b05c9c5e7ea9ab59
Muhammad Muneeb <itsmuneebrehman.conact@gmail.com>
2025-12-22 14:37:16 +0500

    Game event Model and migration added. ALL DB DONE

---
+++ app/Models/GameEvent.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GameEvent extends Model
+{
+    protected $fillable = [
+        'game_id',
+        'event_type',
+        'description',
+        'tag',
+        'team1_points_at_event',
+        'team2_points_at_event',
+    ];
+
+    public function game()
+    {
+        return $this->belongsTo(Game::class);
+    }
+
+}
